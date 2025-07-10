0299606e076e49b41d7b3513da7ef6f7fb3e79a5
vollmond <98267595+vollmond@users.noreply.github.com>
2025-07-10 16:07:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -47,9 +47,8 @@ document.addEventListener('DOMContentLoaded', () => {
         },
         "contacto": {
             title: "Fale Connosco e Encontre-nos",
-            text: "Estamos à sua disposição! Contacte-nos para agendar a sua avaliação ou tratamento através do nosso email:<br><a href='mailto:contacto@bloomestetica.pt' class='email-link'>contacto@bloomestetica.pt</a><br><br><hr class='contact-divider'><a href='https://wa.me/351920743163' target='_blank' class='whatsapp-link'>💬 Falar no WhatsApp</a>"
-            // Deixamos a secção de imagens vazia para o contacto
-            images: [], 
+            text: "Estamos à sua disposição! Contacte-nos para agendar a sua avaliação ou tratamento através do nosso email:<br><a href='mailto:contacto@bloomestetica.pt' class='email-link'>contacto@bloomestetica.pt</a><br><br><hr class='contact-divider'><a href='https://wa.me/351920743163' target='_blank' class='whatsapp-link'>💬 Falar no WhatsApp</a>",
+            images:[],
             // NOVO: Adicionamos o código do mapa aqui
             mapIframe: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d337.9957009252957!2d-8.222045710254251!3d37.09086603509134!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd1acd62ac6fa579%3A0x9c4fc9ee775b5808!2sPESTE%20RIBEIRO%20DESINFESTA%C3%87%C3%95ES!5e1!3m2!1spt-PT!2spt!4v1752068603147!5m2!1spt-PT!2spt" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
         }
